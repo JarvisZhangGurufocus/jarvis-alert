6c5339abde83bf8f4785ca27d41e46d0e45f3bf3
jiawei zhang <zhangjw.uta@gmail.com>
2020-02-12 19:06:31 -0600

    test

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const app = express()
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 
+const RedisSMQ = require("rsmq");
+const rsmq = new RedisSMQ({})
+rsmq.createQueue({ qname: "alert" }, (err, res) => {
+  console.log(err, res)
+})
+
 app.post('/event', jsonParser, (req, res) => {
-  console.log(req.body)
+  rsmq.sendMessage({ qname: "alert", message: 'alert' }, (err, res) => {
+    console.log(err, res)
+  })
   res.json({
     challenge: req.body.challenge
+  })
+})
+
+app.get('/event', (req, res) => {
+  rsmq.popMessage({ qname: "alert" }, (_, text) => {
+    if (text) {
+      res.send(text)
+    }
   })
 })
 
